6aef8e01210abd2c00c37bd8bf2d18f5d27f49e4
Nickolaus Alderete <122177990+Nkalderete@users.noreply.github.com>
2023-06-22 15:11:00 -0500

    Update netscan.py

--- netscan.py
+++ netscan.py
@@ -1,89 +1,33 @@
-#!/usr/bin/python3
-# Nick Alderete & Jeremy Patton
-# Automated network scan & Email notification
+  File "./test2.py", line 58
+    def main():
+    ^
+IndentationError: expected an indented block
+Error in sys.excepthook:
+Traceback (most recent call last):
+  File "/usr/lib/python3/dist-packages/apport_python_hook.py", line 72, in apport_excepthook
+    from apport.fileutils import likely_packaged, get_recent_crashes
+  File "/usr/lib/python3/dist-packages/apport/__init__.py", line 5, in <module>
+    from apport.report import Report
+  File "/usr/lib/python3/dist-packages/apport/report.py", line 32, in <module>
+    import apport.fileutils
+  File "/usr/lib/python3/dist-packages/apport/fileutils.py", line 12, in <module>
+    import os, glob, subprocess, os.path, time, pwd, sys, requests_unixsocket
+  File "/usr/lib/python3/dist-packages/requests_unixsocket/__init__.py", line 1, in <module>
+    import requests
+  File "/usr/lib/python3/dist-packages/requests/__init__.py", line 95, in <module>
+    from urllib3.contrib import pyopenssl
+  File "/usr/lib/python3/dist-packages/urllib3/contrib/pyopenssl.py", line 46, in <module>
+    import OpenSSL.SSL
+  File "/usr/lib/python3/dist-packages/OpenSSL/__init__.py", line 8, in <module>
+    from OpenSSL import crypto, SSL
+  File "/usr/lib/python3/dist-packages/OpenSSL/crypto.py", line 1553, in <module>
+    class X509StoreFlags(object):
+  File "/usr/lib/python3/dist-packages/OpenSSL/crypto.py", line 1573, in X509StoreFlags
+    CB_ISSUER_CHECK = _lib.X509_V_FLAG_CB_ISSUER_CHECK
+AttributeError: module 'lib' has no attribute 'X509_V_FLAG_CB_ISSUER_CHECK'
 
-
-import csv
-import socket
-import smtplib
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
-import time
-
-
-# IP addresses to monitor
-ip_addresses = ['10.0.0.126', '10.0.0.197', '10.0.0.123', '10.0.0.82', '10.0.0.74', '10.0.0.175', '10.0.0.100',
-                '10.0.0.101', '10.0.0.102', '10.0.0.103', '10.0.0.6', '10.0.0.176']
-# Addresses: 
-# .126 = Accounting 1 | .197 = Accounting 2 | .123 = Bobs Analytics server | 
-# .82 = Metasploitable | .74 = Risk Analyst 1 | .175 = Web server | .100/101 = Hunter 1 | 
-# 102/103 = Hunter 2 | .6 = Splunk/ SIEM | .176 = OpenVPN server
-
-
-# Path to save the CSV file
-csv_file = 'traffic_data.csv'
-
-
-# Email details
-sender_email = 'hunter2user@gmail.com'
-sender_password = '401project'
-receiver_email = 'hunter2user@gmail.com'
-smtp_server = 'smtp.gmail.com'
-smtp_port = 587
-
-
-def check_external_traffic(ip_address):
-    try:
-        # Create a socket connection to the IP address on port 80
-        sock = socket.create_connection((ip_address, 80), timeout=5)
-        external_traffic = True
-        sock.close()
-    except (socket.timeout, ConnectionRefusedError):
-        external_traffic = False
-    return external_traffic
-
-
-def save_traffic_data(traffic_data):
-    timestamp = time.strftime('%Y-%m-%d %H:%M:%S')
-    traffic_data.append(timestamp)
-    with open(csv_file, 'a', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerow(traffic_data)
-
-
-def send_email_alert(csv_file):
-    # Create a multipart message
-    message = MIMEMultipart()
-    message['From'] = sender_email
-    message['To'] = receiver_email
-    message['Subject'] = 'External Network Traffic Alert'
-
-    # Attach the CSV file
-    with open(csv_file, 'r') as file:
-        attachment = MIMEText(file.read(), 'csv')
-        attachment.add_header('Content-Disposition', 'attachment', filename=csv_file)
-        message.attach(attachment)
-
-    # Send the email
-    with smtplib.SMTP(smtp_server, smtp_port) as server:
-        server.starttls()
-        server.login(sender_email, sender_password)
-        server.send_message(message)
-
-
-if __name__ == '__main__':
-    while True:
-        # Check for external traffic and save data to CSV
-        for ip_address in ip_addresses:
-            if check_external_traffic(ip_address):
-                save_traffic_data([ip_address, 'External Traffic'])
-
-        # Send email alert with the CSV file attached
-        send_email_alert(csv_file)
-
-        # Wait for a specific interval before running the loop again (e.g., every 5 minutes)
-        time.sleep(300)  # 300 seconds = 5 minutes
-
-
-
-
+Original exception was:
+  File "./test2.py", line 58
+    def main():
+    ^
+IndentationError: expected an indented block
